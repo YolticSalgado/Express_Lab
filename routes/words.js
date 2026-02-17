fa02d9bac86b7133d3dab6742850afe6b314f1ca
Yoltic Salgado <yolticsalgado@gmail.com>
2026-02-17 12:21:05 -0500

    Trying to make WOTD work

--- routes/words.js
+++ routes/words.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();  //Calling express as a function sets up server
+const {readFile, writeFile} = require('fs').promises;
 
 router.get('/', (req, res)=>{  // /words
     res.send('Word Homepage');
-})
+});
 router.get('/wotd', (req, res)=>{   // /words/wotd
-    res.send('Here Is The Word of The Day: ');
-})
+    res.render('wotd')
+});
 
 module.exports = router;
